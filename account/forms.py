3b6d04077e134a95dbb4454a53181ad30fc6dd3f
Stefan <stefanandjelic8@gmail.com>
2022-05-23 01:23:22 -0500

    started project, created account app, first migration

---
+++ account/forms.py
@@ -0,0 +1,8 @@
+from socket import fromshare
+from django import forms
+
+
+# Login form
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
